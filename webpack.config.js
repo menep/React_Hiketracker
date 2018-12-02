2d5ba5758de58dee80f541132810a6e47f9b3d6d
menep <menep@protonmail.com>
2018-12-02 14:27:56 +0100

    corrected webpack issues with entry and outputs

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const webpack = require("webpack");
+const HTMLWPP = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
   mode: "development",
   module: {
     rules: [
@@ -29,21 +29,26 @@ module.exports = {
             loader: "img-loader"
           }
         ]
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader"
       }
     ]
-  },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
   },
   devServer: {
     contentBase: path.join(__dirname, "public/"),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
+    publicPath: "http://localhost:3000/",
     hotOnly: true,
     historyApiFallback: true
   },
   devtool: "cheap-eval-source-map",
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWPP({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
