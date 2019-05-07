2569acae4234a188ae3cd134fd61b5c6c4fef17a
menep <menep@protonmail.com>
2019-05-07 21:12:32 +0200

    update firebase data, gitignore, packages

--- src/assets/js/firebase/firebase.js
+++ src/assets/js/firebase/firebase.js
@@ -1,13 +1,14 @@
-import firebase from "firebase/app";
+import * as firebase from "firebase/app";
 import "firebase/firebase-database";
 
 var config = {
-  apiKey: "AIzaSyB1XDH_k7APcD0ohReKnpiKGC65T2xEczQ",
-  authDomain: "hiketracker-9f581.firebaseapp.com",
-  databaseURL: "https://hiketracker-9f581.firebaseio.com",
-  projectId: "hiketracker-9f581",
-  storageBucket: "hiketracker-9f581.appspot.com",
-  messagingSenderId: "244899251322"
+  apiKey: "AIzaSyD9cM4GxresLN4G4vzb8rOMxpzP5FcnSCM",
+  authDomain: "hiketracker-137fe.firebaseapp.com",
+  databaseURL: "https://hiketracker-137fe.firebaseio.com",
+  projectId: "hiketracker-137fe",
+  storageBucket: "hiketracker-137fe.appspot.com",
+  messagingSenderId: "642683725121",
+  appId: "1:642683725121:web:78117ae962050a8d"
 };
 const fire = firebase.initializeApp(config);
 
